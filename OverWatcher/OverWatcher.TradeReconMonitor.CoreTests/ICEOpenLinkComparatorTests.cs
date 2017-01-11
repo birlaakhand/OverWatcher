8f901961cad08ccb3b2135b486ea178a550ce644
WANG HANPENG <killjaeden@gmail.com>
2017-01-11 10:02:02 +0800

    ffffffffff

---
+++ OverWatcher/OverWatcher.TradeReconMonitor.CoreTests/ICEOpenLinkComparatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OverWatcher.TradeReconMonitor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverWatcher.TradeReconMonitor.Core.Tests
+{
+    [TestClass()]
+    public class ICEOpenLinkComparatorTests
+    {
+        [TestMethod()]
+        public void DiffTest()
+        {
+            PrivateObject diff = new PrivateObject(typeof(ICEOpenLinkComparator));
+
+        }
+    }
+}
